fadf2e7091b6b1d067d0fc12deebabeb9b6b855a
JJ Wang <jonathanjacobwang@gmail.com>
2024-02-01 11:53:17 -0800

    added multer middleware

--- my-backend-app/index.js
+++ my-backend-app/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.send('File uploaded!');
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
